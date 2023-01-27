48a2f50cffd2fe84f795d0f2434ee03dbc6f68f8
墨屿洺 <9083518@qq.com>
2023-01-27 15:43:46 +0800

    vite相关设置完善

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
+import { resolve } from 'path'
 
 
 
@@ -33,10 +34,30 @@ export default (({ command, mode, ssrBuild }: any) => {
         resolvers: [VantResolver()],
       }),
     ],
-    base: env.VITE_BASE_PATH,
+    publicDir: "public",
+    server: {
+      https: false, //(使用https)启用 TLS + HTTP/2。注意：当 server.proxy 选项 也被使用时，将会仅使用 TLS
+      host: "0.0.0.0", // 监听所有地址
+      port: 8080, //指定开发服务器端口：默认3000
+      open: process.platform === 'darwin', //启动时自动在浏览器中打开
+      cors: true, //为开发服务器配置 CORS
+      proxy: {
+        //配置自定义代理规则
+        // 字符串简写写法
+        '/api': {
+          target: '',
+          changeOrigin: true, //是否跨域
+          ws: true,
+          rewrite: path => path.replace(/^\/api/, "")
+        }
+      }
+    },
+    base: './',
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+        '@': resolve(__dirname, 'src'),
+        "@assets": resolve(__dirname, "src/assets"),
+        "@components": resolve(__dirname, "src/components"),
       }
     },
   })
